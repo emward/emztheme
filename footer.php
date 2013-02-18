c2ad190838c81e37afd922c161b239c7716ea08f
emward <eward@mcad.edu>
2013-02-17 20:06:24 -0600

    theme files
    uploading the files for emztheme, my custom theme based on underscores.

---
+++ footer.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package EmzTheme
+ * @since EmzTheme 1.0
+ */
+?>
+
+	</div><!-- #main .site-main -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<h1>Contact Us</h1>
+			<p>Theme Enthusiasts</p>
+			<p>555 Nowhere Avenue South</p>
+			<p>Phone: (800) 555-7882</p>
+	</footer><!-- #colophon .site-footer -->
+</div><!-- #page .hfeed .site -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
